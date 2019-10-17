97e68a850ea369c3d53a42d078717747738e723d
refugiorangel <54346027+refugiorangel@users.noreply.github.com>
2019-10-17 02:08:19 -0500

    Create p9e7.c

---
+++ p9e7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define VALOR_MAX 5
+int main (){
+    int enteroSuma = 0;
+    int enteroNumero = 0;
+    int enteroContador = 0;
+    while (enteroContador < VALOR_MAX){
+        printf("Ingrese un número:");
+        scanf("%d", &enteroNumero);
+        enteroSuma += enteroNumero;
+        enteroContador++;
+        if (enteroSuma > 50){
+            printf("Se rebasó la cantidad límite.\n");
+            break;
+        }
+    }
+    printf("El valor de la suma es: %d\n", enteroSuma);
+    return 0;
+}
