c8ad69a3616ff5b788da9b401d4277a2a2a15831
refugiorangel <54346027+refugiorangel@users.noreply.github.com>
2019-10-17 02:08:44 -0500

    Create p9e8.c

---
+++ p9e8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LIMITE 5
+    int main (){
+    int enteroContador = 1;
+    int enteroNumero = 0;
+    int enteroSuma = 0;
+    while (enteroContador <= LIMITE){
+        printf("Ingrese número par %d:", enteroContador);
+        scanf("%d",&enteroNumero);
+        if (enteroNumero%2 != 0){
+            printf("El número insertado no es par.\n");
+            continue;
+        }
+        enteroSuma += enteroNumero;
+        enteroContador++;
+    }
+    printf("La suma de los números es: %d\n", enteroSuma);
+    return 0;
+}
